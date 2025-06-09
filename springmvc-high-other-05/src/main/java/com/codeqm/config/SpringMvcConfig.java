2c48c640ff639f29d6519a3a9bab59549457cf4d
qingmu@DESKTOP_GW <qingmu_0722@163.com>
2025-06-09 10:12:02 +0800

    feat: 5.2.2 拦截器配置详解

--- springmvc-high-other-05/src/main/java/com/codeqm/config/SpringMvcConfig.java
+++ springmvc-high-other-05/src/main/java/com/codeqm/config/SpringMvcConfig.java
@@ -22,7 +22,27 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     // 添加拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        /*
+         * 多个拦截器执行顺序
+         * 1. preHandle 方法执行顺序：按照添加的顺序依次执行
+         * 2. postHandle 方法执行顺序：按照添加的顺序逆序执行
+         * 3. afterCompletion 方法执行顺序：按照添加的顺序逆序执行
+         */
+
+        // 配置方案 1. 拦截全部请求
         // 将拦截器添加到Springmvc环境, 默认拦截所有Springmvc分发的请求
-        registry.addInterceptor(new MyInterceptor());
+//        registry.addInterceptor(new MyInterceptor());
+
+        // 配置方案 2. 拦截指定请求
+        //   指定地址拦截 .addPathPatterns("/user/test1")
+        //     * 任意一层字符串    ** 表示任意多层字符串
+//        registry.addInterceptor(new MyInterceptor())
+//                .addPathPatterns("/user/**"); // 拦截所有/user/开头的请求
+
+        // 配置方案 3. 排除指定请求
+        //   指定地址排除 .excludePathPatterns("/user/test1")
+        registry.addInterceptor(new MyInterceptor())
+                .addPathPatterns("/user/**")
+                .excludePathPatterns("/user/test2"); // 排除/user/test2请求不被拦截
     }
 }
